d76df50b1faf9812be717985dcf4344ca3fa763f
Yiğitcan Uçan <ucanyiit@gmail.com>
2021-11-16 13:02:22 +0300

    Typescript, @types/react and convert files to .tsx

--- ahc_frontend/src/app/MainStoreContext.tsx
+++ ahc_frontend/src/app/MainStoreContext.tsx
@@ -1,9 +1,9 @@
-import React, {createContext, useContext, useState} from 'react';
+import {createContext, useContext, useState} from 'react';
 import {MainStore} from '../stores/MainStore';
 
-const MainStoreContext = createContext();
+const MainStoreContext = createContext({});
 
-export const MainStoreProvider = ({children}) => {
+export const MainStoreProvider = ({children} : any) => {
   const [mainStore] = useState(() => new MainStore());
   return (
     <MainStoreContext.Provider value={mainStore}>
