3200550c8755d6b41b1ce4c590de943c6679b3ea
DENIZ KOLUACIK <e2310274@ceng.metu.edu.tr>
2021-11-23 20:47:54 +0300

    blacken 😎

--- ahc_backend/ahc/ahc_users/apps.py
+++ ahc_backend/ahc/ahc_users/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AhcUsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ahc_users'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ahc_users"
--- ahc_backend/ahc/ahc_users/models.py
+++ ahc_backend/ahc/ahc_users/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, unique=True, related_name='profile', on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, unique=True, related_name="profile", on_delete=models.CASCADE
+    )
     profile_image = models.ImageField(blank=True, null=True)
