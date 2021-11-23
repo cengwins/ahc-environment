a425fcddbbe54e1bfa8a9bc04f86bdc765ba5e38
Ozan Akın <ozn.akn@gmail.com>
2021-11-23 23:26:27 +0300

    implement user views, add ahc_utils module

---
+++ ahc_backend/ahc_utils/helpers.py
@@ -0,0 +1,42 @@
+from rest_framework.exceptions import APIException
+
+
+class BasicException(APIException):
+    status_code = 400
+    default_code = 'basic_error'
+
+    def __init__(self, code):
+        super().__init__(detail={'code': code})
+
+
+class UnauthorizedException(APIException):
+    status_code = 403
+    default_code = 'unauthorized'
+
+    def __init__(self, code):
+        super().__init__(detail={'code': code})
+
+
+class PostFieldNotExistsException(APIException):
+    status_code = 400
+    default_detail = 'Missing Field'
+    default_code = 'missing_field'
+
+    def __init__(self, field):
+        super().__init__(detail={'code': self.default_code, 'field': field})
+
+
+def check_fields(data, fields):
+    for f in fields:
+        if f not in data:
+            raise PostFieldNotExistsException(f)
+
+    return True
+
+
+def bad_request(code):
+    raise BasicException(code)
+
+
+def unauthorized(code):
+    raise UnauthorizedException(code)
