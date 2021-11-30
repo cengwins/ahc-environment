e9f6a4d7a911400e98d5e4aa5f763d272061a190
Yiğitcan Uçan <ucanyiit@gmail.com>
2021-11-30 19:23:17 +0300

    Using userStore on `Login` and `Register` pages

--- ahc_frontend/src/pages/Login.tsx
+++ ahc_frontend/src/pages/Login.tsx
@@ -3,35 +3,48 @@ import {
 } from 'react-bootstrap';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { useStores } from '../stores/MainStore';
+import Register from './Register';
 
-const Login = () => (
-  <div className="d-flex flex-column min-vh-100">
-    <Header />
-    <Container className="my-5">
-      <Row className="text-start">
-        <Col />
-        <Col sm={4}>
-          {/** Placeholder login form */}
-          <Form className="mt-5">
-            <Form.Group className="mb-3" controlId="email">
-              <Form.Label>Email Address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
+const Login = () => {
+  const { userStore } = useStores();
 
-            <Form.Group className="mb-3" controlId="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Log In
-            </Button>
-          </Form>
-        </Col>
-        <Col />
-      </Row>
-    </Container>
-    <Footer />
-  </div>
-);
+  console.log(userStore);
+  const { username } = userStore;
+  if (!username) {
+    return (
+      <Register />
+    );
+  }
 
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
+      <Container className="my-5">
+        <Row className="text-start">
+          <Col />
+          <Col sm={4}>
+            {/** Placeholder login form */}
+            <Form className="mt-5">
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Log In
+              </Button>
+            </Form>
+          </Col>
+          <Col />
+        </Row>
+      </Container>
+      <Footer />
+    </div>
+  );
+};
 export default Login;
---
+++ ahc_frontend/src/pages/Register.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import {
+  Container, Form, Button, Col, Row,
+} from 'react-bootstrap';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import { useStores } from '../stores/MainStore';
+
+const Register = () => {
+  const { userStore } = useStores();
+
+  console.log(userStore);
+
+  const [mail, setMail] = useState('');
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
+      <Container className="my-5">
+        <Row className="text-start">
+          <Col />
+          <Col sm={4}>
+            <h1 className="mt-5">Register</h1>
+            <Form
+              className="mt-5"
+              onSubmit={(e) => {
+                e.preventDefault();
+                userStore.register(mail, name, surname, password);
+              }}
+            >
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" onChange={(e) => setMail(e.target.value)} />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="surname">
+                <Form.Label>Surname</Form.Label>
+                <Form.Control type="text" placeholder="Surname" onChange={(e) => setSurname(e.target.value)} />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Register
+              </Button>
+            </Form>
+          </Col>
+          <Col />
+        </Row>
+      </Container>
+      <Footer />
+    </div>
+  );
+};
+export default Register;
