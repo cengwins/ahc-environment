a7b41354f384158b7c21f12d8a2dd947c0f2653a
Cenk Arda Yılmaz <e2310670@ceng.metu.edu.tr>
2021-12-21 15:32:44 +0300

    Github Profile model has been created

--- ahc_backend/ahc_github/models.py
+++ ahc_backend/ahc_github/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from ahc_users.models import User
 
-# Create your models here.
+
+class GithubProfile(models.Model):
+    """
+    Model for storing GitHub profiles of users.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    access_token = models.TextField()
+
+    # TODO: add necessary fields to be stored in db
