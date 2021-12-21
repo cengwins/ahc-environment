ad1ecf55c3bda80b25a8a8630f6420baa3c60271
Cenk Arda Yılmaz <e2310670@ceng.metu.edu.tr>
2021-12-21 15:34:35 +0300

    Some helper methods have been added to GithubProfile model class.

--- ahc_backend/ahc_github/models.py
+++ ahc_backend/ahc_github/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ahc_users.models import User
+from github import AuthenticatedUser, Github, PaginatedList, Repository, Branch
 
 
 class GithubProfile(models.Model):
@@ -10,3 +11,18 @@ class GithubProfile(models.Model):
     access_token = models.TextField()
 
     # TODO: add necessary fields to be stored in db
+
+    def get_authenticated_user(self) -> AuthenticatedUser:
+        g = Github(self.access_token)
+        return g.get_user()
+
+    def get_repos(self) -> PaginatedList:
+        return self.get_authenticated_user().get_repos()
+
+    def get_repo(self, repo_name: str) -> Repository:
+        g = Github(self.access_token)
+        return g.get_repo(full_name_or_id=repo_name)
+
+    def get_branch(self, repo_name: str, branch_name: str) -> Branch:
+        g = Github(self.access_token)
+        return g.get_repo(full_name_or_id=repo_name).get_branch(branch_name)
