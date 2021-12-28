2954f7ea1988cf6ede1991779f06ba18b7fa1b1f
DENIZ KOLUACIK <e2310274@ceng.metu.edu.tr>
2021-12-28 22:28:59 +0300

    implement simulation models

--- ahc_backend/ahc_simulations/models.py
+++ ahc_backend/ahc_simulations/models.py
@@ -1,59 +1,68 @@
+import re
+
+from django.core.validators import RegexValidator
 from django.db import models
 
-
-"""
-(DK):
-- sequence_id?
-- The commit field might be redundant. There is already a reference field.
-- Do we support HEAD^1, HEAD~2 etc? It is unlikely any one will ever want to use such a
-thing but we might want to document this for the sake of completeness.
-- Do we have a many-to-one between Repository and Simulation?
-"""
+from ahc_backend.ahc_repositories.models import Repository
 
 
 class Simulation(models.Model):
-    """Model representing a simulation setup.
-
-    Attributes:
-        id: int
-        repository: int
-        sequence_id: int
-        commit: string
-        reference: string (HEAD/main/89F6B6...)
-        reference_type: string (branch/tag/commit)
-        created_at: datetime
-        updated_at: datetime
+    """
+    Model representing a simulation setup.
     """
 
-    pass  # TODO: Fill me with fields.
+    REFERENCE_TYPES = [
+        ("T", "Tag"),
+        ("C", "Commit"),
+        ("B", "Branch"),
+    ]
 
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    # For each repository, subsequent simulations' sequence_id are incremented by one.
+    sequence_id = models.PositiveIntegerField()
+    # Git commits are exactly 40-digit hexadecimal values
+    commit = models.CharField(
+        validators=[RegexValidator(regex=re.compile("^[0-9a-zA-Z]{40}$"))]
+    )
+    reference = models.CharField(max_length=80)
+    reference_type = models.CharField(max_length=1, choices=REFERENCE_TYPES)
+    created_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(null=True)
 
-"""
-(DK):
-- We can clarify what sequence_id is.
-- My understanding is that created/updated_at fields stand for when the user booked a
-simulation run and the last time there was a change on the booking. Is this correct?
-- Do result_status and exit_code signify different information?
-"""
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.sequence_id = (
+                Simulation.objects.filter(repository=self.repository)
+                .aggregate(max_id=models.Max("sequence_id"))
+                .get("max_id", 0)
+                + 1
+            )
+        super().save(*args, **kwargs)
 
 
 class SimulationRun(models.Model):
-    """Statistics of a simulation run.
-
-    Attributes:
-        id: int
-        simulation: int (simulation one-to-many)
-        sequence_id: int
-        started_at: datetime
-        finished_at: datetime
-        result_status: string
-        exit_code: int (process exit code)
-        log_path: path to the log of the simulation run
-        created_at: datetime
-        updated_at: datetime
+    """
+    Statistics of a simulation run.
     """
 
-    pass  # TODO: Fill me with fields.
+    simulation = models.ForeignKey(Simulation, on_delete=models.CASCADE)
+    sequence_id = models.PositiveIntegerField()
+    started_at = models.DateTimeField(null=True)
+    finished_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(null=True)
+    exit_code = models.PositiveIntegerField()
+    log_path = models.CharField(max_length=4096)  # PATH_MAX
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.sequence_id = (
+                SimulationRun.objects.filter(simulation=self.simulation)
+                .aggregate(max_id=models.Max("sequence_id"))
+                .get("max_id", 0)
+                + 1
+            )
+        super().save(*args, **kwargs)
 
 
 """
@@ -70,17 +79,27 @@ complicated and require some research beforehand.
 
 
 class SimulationRunMetric(models.Model):
-    """Model for metrics of a simulation run.
-
-    Attributes:
-        id: int
-        simulation_run: int (simulation one-to-one)
-        type: str
-        name: str
-        value_float: float
-        value_int: int
-        created_at: datetime
     """
+    Model for metrics of a simulation run.
+    """
+
+    simulation_run = models.ForeignKey(SimulationRun, on_delete=models.CASCADE)
+    sequence_id = models.PositiveIntegerField()
+    created_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(null=True)
+    type = models.CharField(choices=[("S", "System"), ("U", "User")], max_length=1)
+    value_float = models.FloatField(null=True)
+    value_int = models.IntegerField(null=True)
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.sequence_id = (
+                SimulationRunMetric.objects.filter(simulation=self.simulation_run)
+                .aggregate(max_id=models.Max("sequence_id"))
+                .get("max_id", 0)
+                + 1
+            )
+        super().save(*args, **kwargs)
 
     pass  # TODO: fill me with fields.
 
@@ -89,44 +108,42 @@ class SimulationRunMetric(models.Model):
 (DK): My understanding is that there is a one-to-many relation between metrics and 
 their aliases. If this is the case alias model and the schema should reflect this.
 Also, It is not very clear to me what updated_at field signifies.
+
+This one will be implemented later (2021-28-12)
+"""
+
+# class SimulationRunMetricAlias(models.Model):
+#     """Aliases for simulation metrics.
+#
+#     Attributes:
+#         id: int
+#         simulation_run: int (simulation one-to-many)
+#         type: string
+#         name: string
+#         alias: string
+#         created_at: datetime
+#         updated_at: datetime
+#     """
+#
+#     pass  # TODO: Fill me with fields.
+#
+
+"""
+This one will be implemented later (2021-28-12)
 """
 
 
-class SimulationRunMetricAlias(models.Model):
-    """Aliases for simulation metrics.
-
-    Attributes:
-        id: int
-        simulation_run: int (simulation one-to-many)
-        type: string
-        name: string
-        alias: string
-        created_at: datetime
-        updated_at: datetime
-    """
-
-    pass  # TODO: Fill me with fields.
-
-
-"""
-(DK):
-- What is the difference between the activity log and the log file pointed by 
-SimulationRun.log_path?
-- What does SimulationActivityLog.target represent?
-- type?
-"""
-
-
-class SimulationActivityLog(models.Model):
-    """TODO: describe me.
-
-    Attributes:
-        user: int (user one-to-many)?
-        simulation: int (simulation ?-to-?)
-        type: string
-        target: string
-        created_at: datetime
-        updated_at: datetime
-    """
-
-    pass  # TODO: Fill me.
+# class SimulationActivityLog(models.Model):
+#     """TODO: describe me.
+#
+#     Attributes:
+#         user: int (user one-to-many)?
+#         simulation: int (simulation ?-to-?)
+#         type: string
+#         target: string
+#         created_at: datetime
+#         updated_at: datetime
+#     """
+#     user = models.ForeignKey(ahc_users.models.User)
+#     simulation = models.ForeignKey(Simulation)
+#
