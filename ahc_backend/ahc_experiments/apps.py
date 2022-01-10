db5228409edc04275a69c2f1c75d3105316dffb5
OZAN AKIN <e2309599@ceng.metu.edu.tr>
2022-01-10 17:43:31 +0300

    fix: import signals before everything

--- ahc_backend/ahc_experiments/apps.py
+++ ahc_backend/ahc_experiments/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class AhcExperimentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "ahc_experiments"
+
+    def ready(self) -> None:
+        from . import signals
+
+        return super().ready()
--- ahc_backend/ahc_users/apps.py
+++ ahc_backend/ahc_users/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class AhcUsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "ahc_users"
+
+    def ready(self) -> None:
+        from . import signals
+
+        return super().ready()
