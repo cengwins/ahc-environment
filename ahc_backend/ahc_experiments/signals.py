cefe838c1c0de0428b8540a848591d29afa2b1e9
OZAN AKIN <e2309599@ceng.metu.edu.tr>
2022-01-12 13:35:36 +0300

    fix(backend): add timeout to experiment create signal

--- ahc_backend/ahc_experiments/signals.py
+++ ahc_backend/ahc_experiments/signals.py
@@ -9,4 +9,4 @@ from .tasks import run_experiment
 @receiver(post_save, sender=Experiment)
 def on_experiment_save(sender, instance: Experiment, created: bool, **kwargs):
     if created:
-        run_experiment.delay(instance.id)
+        run_experiment.apply_async(args=[instance.id], countdown=5)
