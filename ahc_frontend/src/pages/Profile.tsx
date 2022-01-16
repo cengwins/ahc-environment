adf5f7a1532c420a506247a9cb978e81a49e796e
Yiğitcan Uçan <ucanyiit@gmail.com>
2022-01-16 15:08:05 +0300

    feature(frontend): Profile page connection

--- ahc_frontend/src/pages/Profile.tsx
+++ ahc_frontend/src/pages/Profile.tsx
@@ -1,23 +1,57 @@
 import { observer } from 'mobx-react';
+import { useState } from 'react';
 import {
+  Button,
   Container,
 } from 'react-bootstrap';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 import { useStores } from '../stores/MainStore';
+
+const ProfileField = (title: string, value: string) => (
+  <div>
+    <span>{`${title}: `}</span>
+    <span>{value}</span>
+  </div>
+);
 
 const Profile = observer(() => {
   const { userStore } = useStores();
+  const [loading, setLoading] = useState(false);
+  const [failedToLoad, setFailed] = useState(false);
 
-  if (!userStore.username) {
-    userStore.getProfile();
+  const {
+    username, email, name, surname,
+  } = userStore;
+
+  if (!loading && !failedToLoad && !username) {
+    setLoading(true);
+    userStore.getProfile()
+      .catch(() => setFailed(true))
+      .finally(() => setLoading(false));
   }
 
   return (
     <div className="d-flex flex-column min-vh-100">
       <Header />
       <Container className="my-5">
-        {userStore.username}
+        <h1 className="mt-5">
+          Profile
+        </h1>
+        <div className="mt-5">
+          <Loading loading={loading} failed={failedToLoad} />
+          {!loading && !failedToLoad && (
+          <div>
+            {ProfileField('Username', username)}
+            {ProfileField('Email', email)}
+            {ProfileField('Name', `${name} ${surname}`)}
+            <Button className="mt-4">
+              Reset Password
+            </Button>
+          </div>
+          )}
+        </div>
       </Container>
       <Footer />
     </div>
