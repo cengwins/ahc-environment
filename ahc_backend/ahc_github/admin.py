f10f7f36e4179ddd5002edd116b27a105a006d0f
Cenk Arda Yılmaz <e2310670@ceng.metu.edu.tr>
2022-03-29 23:24:50 +0300

    add import/export feature for ahc_experiments and ahc_github modules

--- ahc_backend/ahc_github/admin.py
+++ ahc_backend/ahc_github/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(GithubProfile)
-admin.site.register(GithubRepositoryDeployToken)
+
+class GithubProfileResource(resources.ModelResource):
+    class Meta:
+        model = GithubProfile
+
+
+class GithubProfileAdmin(ImportExportModelAdmin):
+    resource_class = GithubProfileResource
+
+
+class GithubRepositoryDeployTokenResource(resources.ModelResource):
+    class Meta:
+        model = GithubRepositoryDeployToken
+
+
+class GithubRepositoryDeployTokenAdmin(ImportExportModelAdmin):
+    resource_class = GithubRepositoryDeployTokenResource
+
+
+admin.site.register(GithubProfile, GithubProfileAdmin)
+admin.site.register(GithubRepositoryDeployToken, GithubRepositoryDeployTokenAdmin)
