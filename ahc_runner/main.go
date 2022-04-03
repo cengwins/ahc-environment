0ba87a367230723f4721f86a083f4abf4e14cccf
OZAN AKIN <e2309599@ceng.metu.edu.tr>
2022-04-03 20:29:33 +0300

    feat(runner): implement more daemon based ahc runner with results

--- ahc_runner/main.go
+++ ahc_runner/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -19,7 +21,6 @@ import (
 	docker "github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/archive"
 	jsonmessage "github.com/docker/docker/pkg/jsonmessage"
-	"github.com/docker/docker/pkg/term"
 	git "github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/google/uuid"
@@ -55,42 +56,51 @@ type AHCConfiguration struct {
 }
 
 type RunnerResponse struct {
-	Name string
+	Name string `json:"name"`
 }
 
 type RepositoryResponse struct {
-	Id       int
-	Slug     int
-	Name     string
-	Upstream string
+	Id       int    `json:"id"`
+	Slug     int    `json:"slug"`
+	Name     string `json:"name"`
+	Upstream string `json:"upstream"`
 }
 
 type ExperimentResponse struct {
-	Id          int
-	Sequence_id int
-	Commit      string
-	Repository  RepositoryResponse
+	Id         int                `json:"id"`
+	SequenceID int                `json:"sequence_id"`
+	Commit     string             `json:"commit"`
+	Repository RepositoryResponse `json:"repository"`
 }
 
-var volumeId string
+type SubmitRunnerJobRequestRun struct {
+	StartedAt  string `json:"started_at"`
+	FinishedAt string `json:"finished_at"`
+	Logs       string `json:"logs"`
+	ExitCode   int    `json:"exit_code"`
+}
+
+type SubmitRunnerJobRequest struct {
+	ExperimentId int                         `json:"experiment_id"`
+	Runs         []SubmitRunnerJobRequestRun `json:"runs"`
+}
+
+var serverUrl string
+var serverSecret string
 var containerBindPath string
 var hostBindPath string
-var containerVolumePath string
-var hostVolumePath string
 
-var ahcConfiguration AHCConfiguration
 var dockerClient docker.APIClient
 
-func clone(url string) error {
+func clone(containerVolumePath string, url string, outStream io.Writer) error {
 	repo, err := git.PlainClone(containerVolumePath, false, &git.CloneOptions{
 		InsecureSkipTLS: true,
 		Depth:           1,
 		URL:             url,
 		ReferenceName:   plumbing.NewBranchReferenceName("main"),
 		SingleBranch:    true,
-		Progress:        os.Stdout,
+		Progress:        outStream,
 	})
-
 	if err != nil {
 		return err
 	}
@@ -105,11 +115,10 @@ func clone(url string) error {
 	return nil
 }
 
-func printJsonStreamToScreen(rd io.ReadCloser) {
+func printJsonStreamToScreen(wr io.Writer, rd io.ReadCloser) {
 	defer rd.Close()
 
-	termFd, isTerm := term.GetFdInfo(os.Stdout)
-	jsonmessage.DisplayJSONMessagesStream(rd, os.Stdout, termFd, isTerm, nil)
+	jsonmessage.DisplayJSONMessagesStream(rd, wr, 0, false, nil)
 }
 
 func initDocker() error {
@@ -122,7 +131,19 @@ func initDocker() error {
 	return nil
 }
 
-func readConfig() (AHCConfiguration, error) {
+func makeRequest(req *http.Request) (*http.Response, error) {
+	client := http.Client{}
+	req.Header = http.Header{
+		"Authorization": []string{fmt.Sprintf("Basic %s", serverSecret)},
+		"Content-Type":  []string{"application/json"},
+	}
+
+	res, err := client.Do(req)
+
+	return res, err
+}
+
+func readConfig(containerVolumePath string) (AHCConfiguration, error) {
 	buf, err := ioutil.ReadFile(path.Join(containerVolumePath, AHC_FILENAME))
 	if err != nil {
 		return AHCConfiguration{}, err
@@ -137,21 +158,21 @@ func readConfig() (AHCConfiguration, error) {
 	return c, nil
 }
 
-func pullImage(image string) error {
+func pullImage(image string, outStream io.Writer) error {
 	ctx := context.Background()
 	res, err := dockerClient.ImagePull(ctx, image, types.ImagePullOptions{})
 	if err != nil {
 		return err
 	}
 
-	printJsonStreamToScreen(res)
+	printJsonStreamToScreen(outStream, res)
 
 	return nil
 }
 
-func buildImage(path string) (string, error) {
+func buildImage(contextPath string, outStream io.Writer) (string, error) {
 	ctx := context.Background()
-	tar, err := archive.TarWithOptions(path, &archive.TarOptions{})
+	tar, err := archive.TarWithOptions(contextPath, &archive.TarOptions{})
 	if err != nil {
 		return "", err
 	}
@@ -170,12 +191,12 @@ func buildImage(path string) (string, error) {
 		return "", err
 	}
 
-	printJsonStreamToScreen(res.Body)
+	printJsonStreamToScreen(outStream, res.Body)
 
 	return imageId, nil
 }
 
-func runContainer(image string, command string, env []string) error {
+func runContainer(hostVolumePath string, image string, command string, env []string) (string, error) {
 	ctx := context.Background()
 	c, err := dockerClient.ContainerCreate(
 		ctx,
@@ -192,13 +213,13 @@ func runContainer(image string, command string, env []string) error {
 					Source: hostVolumePath,
 					Target: "/app",
 				},
-				{
+				/* {
 					Type:   mounttypes.TypeBind,
 					Source: "/dev/bus/usb",
 					Target: "/dev/bus/usb",
-				},
+				}, */
 			},
-			Privileged: true,
+			// Privileged: true,
 		},
 		nil,
 		nil,
@@ -206,13 +227,13 @@ func runContainer(image string, command string, env []string) error {
 	)
 	if err != nil {
 		fmt.Printf("%s\n", err)
-		return err
+		return "", err
 	}
 
 	err = dockerClient.ContainerStart(ctx, c.ID, types.ContainerStartOptions{})
 	if err != nil {
 		fmt.Printf("%s\n", err)
-		return err
+		return "", err
 	}
 
 	reader, err := dockerClient.ContainerLogs(context.Background(), c.ID, types.ContainerLogsOptions{
@@ -221,49 +242,52 @@ func runContainer(image string, command string, env []string) error {
 		Follow:     true,
 	})
 	if err != nil {
-		return err
+		return "", err
 	}
-
 	defer reader.Close()
+
 	content, _ := ioutil.ReadAll(reader)
-	fmt.Print(string(content))
 
 	err = dockerClient.ContainerRemove(ctx, c.ID, types.ContainerRemoveOptions{})
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	return nil
+	return string(content), nil
 }
 
-func runJob(upstream_url string) error {
-	volumeId = uuid.NewString()
-	containerVolumePath = fmt.Sprintf("%s/%s", containerBindPath, volumeId)
-	hostVolumePath = fmt.Sprintf("%s/%s", hostBindPath, volumeId)
+func runJob(upstream_url string) ([]SubmitRunnerJobRequestRun, error) {
+	var resultBuffer bytes.Buffer
+
+	result := make([]SubmitRunnerJobRequestRun, 0)
+
+	volumeId := uuid.NewString()
+	containerVolumePath := fmt.Sprintf("%s/%s", containerBindPath, volumeId)
+	hostVolumePath := fmt.Sprintf("%s/%s", hostBindPath, volumeId)
 
 	fmt.Printf("Using directory %s\n", containerVolumePath)
 
-	err := clone(upstream_url)
+	err := clone(containerVolumePath, upstream_url, &resultBuffer)
 	if err != nil {
-		return err
+		return result, err
 	}
 
-	config, err := readConfig()
+	config, err := readConfig(containerVolumePath)
 	if err != nil {
-		return err
+		return result, err
 	}
 
 	containerImage := config.Image
 
 	if containerImage == "." {
-		containerImage, err = buildImage(containerVolumePath)
+		containerImage, err = buildImage(containerVolumePath, &resultBuffer)
 		if err != nil {
-			return err
+			return result, err
 		}
 	} else {
-		err = pullImage(config.Image)
+		err = pullImage(config.Image, &resultBuffer)
 		if err != nil {
-			return err
+			return result, err
 		}
 	}
 
@@ -282,9 +306,17 @@ func runJob(upstream_url string) error {
 	if len(config.Experiment.Runs) == 0 {
 		fmt.Printf("No run configuration found for experiment, exiting...\n")
 
-		return nil
+		return result, err
 	}
 
+	totalRuns := 0
+	for _, run := range config.Experiment.Runs {
+		totalRuns += run.SamplingCount
+	}
+
+	result = make([]SubmitRunnerJobRequestRun, totalRuns)
+
+	k := 0
 	for _, run := range config.Experiment.Runs {
 		fmt.Printf("Running for run %s with total sampling with %d\n", run.Name, run.SamplingCount)
 
@@ -295,104 +327,137 @@ func runJob(upstream_url string) error {
 
 			env[ahc_run_seq_env_index] = fmt.Sprintf("AHC_RUN_SEQ=%d", i)
 
-			err = runContainer(containerImage, config.Command, env)
+			startTime := time.Now()
+			logs, err := runContainer(hostVolumePath, containerImage, config.Command, env)
 			if err != nil {
-				return err
+				return result, err
 			}
+			endTime := time.Now()
+
+			result[k] = SubmitRunnerJobRequestRun{
+				StartedAt:  startTime.Format("2006-01-02 15:04:05.000000"),
+				FinishedAt: endTime.Format("2006-01-02 15:04:05.000000"),
+				ExitCode:   0,
+				Logs:       fmt.Sprintf("%s\n%s\n", resultBuffer.String(), logs),
+			}
+			k += 1
 		}
 	}
 
-	return nil
+	return result, nil
 }
 
-func checkRunner(connect_url string, connect_secret string) bool {
-	connect_path := fmt.Sprintf("http://%s/api/runner/", connect_url)
+func checkRunner() error {
+	connect_path := fmt.Sprintf("http://%s/api/runner/", serverUrl)
 
-	client := http.Client{}
 	req, err := http.NewRequest("GET", connect_path, nil)
 	if err != nil {
-		return false
+		return err
 	}
 
-	req.Header = http.Header{
-		"Authorization": []string{fmt.Sprintf("Basic %s", connect_secret)},
-	}
-
-	res, err := client.Do(req)
+	res, err := makeRequest(req)
 	if err != nil {
-		return false
+		return err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
 		fmt.Println("Could not connect to server, exiting...")
-		return false
+		return errors.New("Could not connect to server")
 	}
 
 	var data RunnerResponse
 	err = json.NewDecoder(res.Body).Decode(&data)
 	if err != nil {
-		return false
+		return err
 	}
 
 	fmt.Printf("Connected with runner %s\n", data.Name)
 
-	return true
+	return nil
 }
 
-func checkForNewJob(connect_url string, connect_secret string) bool {
-	connect_path := fmt.Sprintf("http://%s/api/jobs/", connect_url)
+func submitRunnerJobResult(experimentId int, result []SubmitRunnerJobRequestRun) error {
+	data := SubmitRunnerJobRequest{
+		ExperimentId: experimentId,
+		Runs:         result,
+	}
+	body, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
 
-	client := http.Client{}
+	connect_path := fmt.Sprintf("http://%s/api/runner/submit/", serverUrl)
+
+	req, err := http.NewRequest("POST", connect_path, bytes.NewReader(body))
+	if err != nil {
+		return err
+	}
+
+	res, err := makeRequest(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		fmt.Println("Could not connect to server, exiting...")
+		return errors.New("Could not connect to server")
+	}
+
+	return nil
+}
+
+func checkForNewJob() error {
+	connect_path := fmt.Sprintf("http://%s/api/runner/jobs/", serverUrl)
+
 	req, err := http.NewRequest("GET", connect_path, nil)
 	if err != nil {
-		return false
+		return err
 	}
 
-	req.Header = http.Header{
-		"Authorization": []string{fmt.Sprintf("Basic %s", connect_secret)},
-	}
-
-	res, err := client.Do(req)
+	res, err := makeRequest(req)
 	if err != nil {
-		return false
+		return err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 && res.StatusCode != 204 {
 		fmt.Println("Could not connect to server, exiting...")
-		return false
+		return errors.New("Could not connect to server")
 	}
 
 	if res.StatusCode == 204 {
 		fmt.Println("No new job found, skipping...")
-		return true
+		return nil
 	}
 
 	var data ExperimentResponse
 	json.NewDecoder(res.Body).Decode(&data)
 	if err != nil {
-		return false
+		return err
 	}
 
 	fmt.Printf("Found job from repository %s from upstream %s commit %s\n", data.Repository.Name, data.Repository.Upstream, data.Commit)
 
-	err = runJob(data.Repository.Upstream)
+	result, err := runJob(data.Repository.Upstream)
 	if err != nil {
-		return false
+		return err
 	}
 
-	return true
+	submitRunnerJobResult(data.Id, result)
+
+	return nil
 }
 
-func startDaemon(connect_url string, connect_secret string) {
-	r := checkRunner(connect_url, connect_secret)
+func startDaemon() {
+	r := checkRunner()
 
-	if r {
+	if r == nil {
 		for range time.Tick(5 * time.Second) {
-			r = checkForNewJob(connect_url, connect_secret)
+			r = checkForNewJob()
 
-			if !r {
+			if r != nil {
 				break
 			}
 		}
@@ -453,7 +518,7 @@ func main() {
 				Action: func(c *cli.Context) error {
 					upstream_url := c.String("url")
 
-					err := runJob(upstream_url)
+					_, err := runJob(upstream_url)
 					if err != nil {
 						fmt.Printf("%s\n", err)
 					}
@@ -475,12 +540,12 @@ func main() {
 					},
 				},
 				Action: func(c *cli.Context) error {
-					connect_url := c.String("url")
-					connect_secret := c.String("secret")
+					serverUrl = c.String("url")
+					serverSecret = c.String("secret")
 
-					fmt.Printf("Starting daemon for %s\n", connect_url)
+					fmt.Printf("Starting daemon for %s\n", serverUrl)
 
-					startDaemon(connect_url, connect_secret)
+					startDaemon()
 
 					return nil
 				},
