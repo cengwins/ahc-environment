b911d8f411cb22d09b3f283ee68586e8665c3711
Cenk Arda Yılmaz <e2310670@ceng.metu.edu.tr>
2022-04-05 17:15:04 +0300

    import-export and filter/search fields are added to ahc_repositories module in admin page.

--- ahc_backend/ahc_repositories/admin.py
+++ ahc_backend/ahc_repositories/admin.py
@@ -1,7 +1,46 @@
 from django.contrib import admin
 
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(Repository)
-admin.site.register(RepositoryUser)
-admin.site.register(RepositoryEnvVariable)
+
+class AHCRepositoryResource(resources.ModelResource):
+    class Meta:
+        model = Repository
+
+
+class AHCRepositoryAdmin(ImportExportModelAdmin):
+    resource_class = AHCRepositoryResource
+    list_display = ('name', 'description', 'url', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('name', 'description', 'url')
+
+
+class AHCRepositoryUserResource(resources.ModelResource):
+    class Meta:
+        model = RepositoryUser
+
+
+class AHCRepositoryUserAdmin(ImportExportModelAdmin):
+    resource_class = AHCRepositoryUserResource
+    list_display = ('user', 'repository', 'created_at', 'updated_at', 'type')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('user', 'repository')
+
+
+class AHCRepositoryEnvVariableResource(resources.ModelResource):
+    class Meta:
+        model = RepositoryEnvVariable
+
+
+class AHCRepositoryEnvVariableAdmin(ImportExportModelAdmin):
+    resource_class = AHCRepositoryEnvVariableResource
+    list_display = ('repository', 'key', 'value', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('repository', 'key', 'value')
+
+
+admin.site.register(Repository, AHCRepositoryAdmin)
+admin.site.register(RepositoryUser, AHCRepositoryUserAdmin)
+admin.site.register(RepositoryEnvVariable, AHCRepositoryEnvVariableAdmin)
