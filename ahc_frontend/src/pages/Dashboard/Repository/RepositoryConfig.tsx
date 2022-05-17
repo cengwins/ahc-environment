8374c206d70617a98caccaa997b5c12bf92453d3
Yiğitcan Uçan <ucanyiit@gmail.com>
2022-05-17 21:17:44 +0300

    feature(frontend): Add scrollable ahc.yml and placeholder save button

--- ahc_frontend/src/pages/Dashboard/Repository/RepositoryConfig.tsx
+++ ahc_frontend/src/pages/Dashboard/Repository/RepositoryConfig.tsx
@@ -2,7 +2,7 @@ import { observer } from 'mobx-react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
-  Button, Card, CardContent, Typography,
+  Button, Card, CardContent, Stack, Typography,
 } from '@mui/material';
 import { blue } from '@mui/material/colors';
 import CodeEditor from '@uiw/react-textarea-code-editor';
@@ -41,7 +41,7 @@ const RepositoryConfig = observer(({ repository }: {repository: RepositoryInfo})
     <div>
       <Typography component="h3" variant="h4" sx={{ my: 2, color: `${blue[700]}` }}>ahc.yml</Typography>
       <Card variant="outlined" sx={{ my: 2, backgroundColor: '#FDFCFD' }}>
-        <CardContent sx={{ padding: '12px 20px' }}>
+        <CardContent sx={{ maxHeight: '70vh', overflow: 'scroll' }}>
           <CodeEditor
             value={ahcYAMLContentEditing}
             language="yaml"
@@ -49,19 +49,28 @@ const RepositoryConfig = observer(({ repository }: {repository: RepositoryInfo})
             onChange={(e) => setAhcYAMLContentEditing(e.target.value)}
             padding={15}
             style={{
-              fontSize: 12,
-              backgroundColor: '#f5f5f5',
+              fontSize: 18,
+              backgroundColor: '#FDFCFD',
               fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
             }}
           />
         </CardContent>
       </Card>
-      <Button
-        variant="contained"
-        onClick={() => setAhcYAMLContent(ahcYAMLContentEditing)}
-      >
-        Display
-      </Button>
+      <Stack direction="row" spacing={2} justifyContent="flex-end">
+        <Button
+          variant="contained"
+          onClick={() => setAhcYAMLContent(ahcYAMLContentEditing)}
+        >
+          Display
+        </Button>
+        <Button
+          color="success"
+          variant="contained"
+          onClick={() => (console.log('not implemented'))}
+        >
+          Save
+        </Button>
+      </Stack>
       <TopologyConfig
         ahcYAML={ahcYAMLContent}
         ahcYAMLEditing={ahcYAMLContentEditing}
