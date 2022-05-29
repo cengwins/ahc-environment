b23cd5c35a7d7c473e217f7859bcf684d8bfd236
Yiğitcan Uçan <ucanyiit@gmail.com>
2022-05-29 15:44:35 +0300

    feature(frontend): Add features to home page, and refactor sections on Home page

---
+++ ahc_frontend/src/svgs/index.tsx
@@ -0,0 +1,9 @@
+import TopologyManagement from './Topology_Management.svg';
+import DetailedMetrics from './Detailed_Metrics.svg';
+import ClassroomManagement from './Classroom_Management.svg';
+import GitHubIntegration from './GitHub_Integration.svg';
+
+export const TopologyManagementIcon = TopologyManagement;
+export const DetailedMetricsIcon = DetailedMetrics;
+export const ClassroomManagementIcon = ClassroomManagement;
+export const GitHubIntegrationIcon = GitHubIntegration;
