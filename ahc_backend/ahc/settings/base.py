4278ba56e77f585ff18fbab1092d4ed1bf870a03
Ozan Akın <ozn.akn@gmail.com>
2023-01-16 20:57:25 +0300

    fix: s3 env variables

--- ahc_backend/ahc/settings/base.py
+++ ahc_backend/ahc/settings/base.py
@@ -124,7 +124,7 @@ USE_TZ = True
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_S3_ACCESS_KEY_ID = os.environ.get("AWS_S3_ACCESS_KEY_ID", "ahc")
 AWS_S3_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_SECRET_ACCESS_KEY", "ahc")
-AWS_S3_ENDPOINT_URL = "https://localhost:9000/"
+AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL", "https://localhost:9000/")
 AWS_S3_VERIFY = True
 
 STATIC_URL = "static/"
